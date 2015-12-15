3580135476842978cc192fa8edd7d108bff94b9c
Proger666 <knorra@mail.ru>
2015-12-15 20:57:52 +0300

    Nice n clear

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,9 @@
 package com.company;
 
-import javax.tools.JavaCompiler;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class Main {
     static long sum = 0;
@@ -22,9 +20,11 @@ public class Main {
 
 
         System.out.println("Lets copy only .txt files and directory structure \n");
-        copyDir(srcDir, dstDir);
+        long startTime = System.currentTimeMillis();
+        copyDir(srcDir, dstDir, ".txt");
+        long stopTime = System.currentTimeMillis();
 
-        System.out.println("Seems its copied. Lets check...");
+        System.out.println("Seems its copied in " + ((stopTime - startTime) / 1000f) + "s. Lets check...");
 
 
         //Cant change link but be able to change object by link
@@ -42,52 +42,57 @@ public class Main {
         srcDirMeasureThread.join();
         dstDirMeasureThread.join();
 
-        System.out.println("Source directory size is: " + DirsSizes[0] / 1024 / 1024);
-        System.out.println("Copied directory size is: " + DirsSizes[1] / 1024 / 1024);
+        System.out.println("Source directory size is: " + DirsSizes[0] / 1024 / 1024f + " mb");
+        System.out.println("Copied directory size is: " + DirsSizes[1] / 1024 / 1024f + " mb");
 
 
-        // write your code here
+
     }
 
-    private static void copyDir(File srcDir, File dstDir) {
+    private static void copyDir(File srcDir, File dstDir, String s) {
         try {
             if (srcDir.isDirectory()) {
                 if (!dstDir.exists())
                     dstDir.mkdir();
             } else if (srcDir.isFile()) {
                 if (!dstDir.exists())
-                    copyFile(srcDir, dstDir);
+                    copyFile(srcDir, dstDir, s);
             }
-
-
-            for (File srcSubDir : srcDir.listFiles()) {
-                String subDirName = srcSubDir.getName();
-                copyDir(srcSubDir, new File(dstDir, subDirName));
-                if (srcSubDir.isFile()) {
-                    copyFile(srcSubDir, new File(dstDir, subDirName));
+            if (srcDir.isDirectory()) {
+                for (File srcSubDir : srcDir.listFiles()) {
+                    String subDirName = srcSubDir.getName();
+                    copyDir(srcSubDir, new File(dstDir, subDirName), ".txt");
+                    if (srcSubDir.isFile()) {
+                        copyFile(srcSubDir, new File(dstDir, subDirName), s);
+                    }
                 }
+
+
             }
         } catch (NullPointerException ex) {
-            System.out.println("last file in dir " + srcDir.getName());
+            System.out.println("We screwed with dir " + srcDir.getName());
         }
     }
 
-    private static void copyFile(File srcDir, File dstDir) {
 
-        int count = 0;
+    private static void copyFile(File srcDir, File dstDir, String s) {
 
-        try (FileInputStream in = new FileInputStream(srcDir);
-             FileOutputStream out = new FileOutputStream(dstDir,false)) {
+        int count;
+        if (srcDir.getName().toLowerCase().contains(s)) {
+
+            try (FileInputStream in = new FileInputStream(srcDir);
+                 FileOutputStream out = new FileOutputStream(dstDir, false)) {
 
 
                 byte[] buff = new byte[64 * 1024];
                 while ((count = in.read(buff)) != -1) {
-                    out.write(buff,0,count);
+                    out.write(buff, 0, count);
                 }
                 out.flush();
 
-        } catch (IOException ex) {
-            System.out.println("We screwed at copyFile, Files is " + srcDir.getName() + " and " + dstDir.getName());
+            } catch (IOException ex) {
+                System.out.println("We screwed at copyFile, Files is " + srcDir.getName() + " and " + dstDir.getName());
+            }
         }
     }
 
